a0784a576e204f067905ee28636b0008cb077e61
dustinpfister <dustin.pfister@gmail.com>
2018-08-13 09:57:57 -0400

    walk html only

--- commands/generate.js
+++ commands/generate.js
@@ -1,16 +1,50 @@
 let klaw = require('klaw'),
-through2 = require('through2');
-
-
+through2 = require('through2'),
+path = require('path');
 
 exports.command = 'generate';
 
-exports.describe = 'generate a report'
+exports.describe = 'generate a report';
 
 exports.handler = function (argv) {
 
-    console.log('generating');
-	console.log(argv.target);
-	console.log(argv);
+    let dir = path.resolve(argv.target);
+
+    console.log('generating report for content at: ' + dir);
+
+    klaw(dir)
+
+    // filter out dirs
+    .pipe(through2.obj(function (item, enc, next) {
+
+            if (item.stats.isFile()) {
+
+                this.push(item);
+
+            }
+            next();
+
+        }))
+
+    // html only
+    .pipe(through2.obj(function (item, enc, next) {
+
+            let ext = path.extname(item.path).toLowerCase();
+
+            if (ext === '.html' || ext === '.htm') {
+
+                this.push(item);
+
+            }
+            next();
+
+        }))
+
+    .on('data', function (item) {
+
+        console.log(path.basename(item.path));
+        //console.log(item)
+
+    })
 
 };
